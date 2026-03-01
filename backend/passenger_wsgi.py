e437173726105442ed57e21d202d2f131edb0fd1
Ricardo A. Sanjur G. <91886986+Ricky06202@users.noreply.github.com>
2026-03-01 08:27:34 -0500

    arreglamos el backend

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,8 +1,12 @@
 import os
 import sys
+from a2wsgi import ASGIMiddleware
 
 # Esto le dice a cPanel dónde está tu código
 sys.path.insert(0, os.path.dirname(__file__))
 
 # Importamos tu app de FastAPI (asumiendo que tu archivo es main.py)
-from main import app as application
+from main import app
+
+# Envolvemos la app para que sea compatible con Passenger (WSGI)
+application = ASGIMiddleware(app)
