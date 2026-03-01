3000297d177750b811db3a4d5578f1281d589d63
Ricardo A. Sanjur G. <91886986+Ricky06202@users.noreply.github.com>
2026-03-01 14:27:12 -0500

    volvemos a exportar application

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,8 +1,12 @@
-def application(environ, start_response):
-    status = '200 OK'
-    text = '¡Hola Mundo! El Python 3.9 en cPanel esta VIVO.'
-    output = text.encode('utf-8')
-    response_headers = [('Content-Type', 'text/plain; charset=utf-8'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-    return [output]
+import os
+import sys
+
+# 1. Agregamos el directorio actual al Path de Python
+sys.path.insert(0, os.path.dirname(__file__))
+
+# 2. Importamos el adaptador y tu app
+from a2wsgi import ASGIMiddleware
+from main import app  # Donde 'main' es main.py y 'app' es FastAPI()
+
+# 3. El objeto que busca Passenger
+application = ASGIMiddleware(app)
