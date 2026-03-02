518b5433537f491fae27b2ab619680765054afa8
Ricardo A. Sanjur G. <91886986+Ricky06202@users.noreply.github.com>
2026-03-02 12:00:41 -0500

    probando

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,12 +1,3 @@
-import os
-import sys
-
-# 1. Agregamos el directorio actual al Path de Python
-sys.path.insert(0, os.path.dirname(__file__))
-
-# 2. Importamos el adaptador y tu app
-from a2wsgi import ASGIMiddleware
-from main import app  # Donde 'main' es main.py y 'app' es FastAPI()
-
-# 3. El objeto que busca Passenger
-application = ASGIMiddleware(app)
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [b"Hola Mundo desde Passenger WSGI!"]
