e5bdb8874ab9dd7d466a09cd13f92efaf3e16bcf
Ricardo A. Sanjur G. <91886986+Ricky06202@users.noreply.github.com>
2026-03-02 12:04:01 -0500

    el anterior funciono solo pa q sepas

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,3 +1,14 @@
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b"Hola Mundo desde Passenger WSGI!"]
+import os
+import sys
+
+# 1. Agregamos el directorio actual al Path de Python
+sys.path.insert(0, os.path.dirname(__file__))
+
+# 2. Importamos el adaptador y tu app
+# Renombramos 'app' a 'fastapi_app' para evitar que Passenger lo encuentre por error
+from a2wsgi import ASGIMiddleware
+from main import app as fastapi_app
+
+# 3. El objeto que busca Passenger (WSGI)
+# Al no existir una variable llamada 'app', Passenger se ve obligado a usar 'application'
+application = ASGIMiddleware(fastapi_app)
